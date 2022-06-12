7becbff71d19cf3412c35b80a1d6bb9816fb2de7
federico.rojas.121 <federico.rojas.121@gmail.com>
2022-06-11 21:56:29 -0300

    init api REST - create, update & delete User implementation

--- src/main/java/com/fundamentos/springboot/fundamentos/service/UserService.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/service/UserService.java
@@ -28,4 +28,21 @@ public class UserService {
     public List<User> getAllUsers(){
         return  userRepository.findAll();
     }
+
+    public Object save(User newUser) {
+        return userRepository.save(newUser);
+    }
+    public void delete(Long id) {
+        userRepository.delete(new User(id));
+    }
+
+    public User update(User newUser, Long id) {
+        return userRepository.findById(id)
+                .map( user -> {
+                    user.setEmail(newUser.getEmail());
+                    user.setBirthdate(newUser.getBirthday());
+                    user.setName(newUser.getName());
+                    return userRepository.save(user);
+                }).get();
+    }
 }
